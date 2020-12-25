6b5b0f1cc50049c4fe4339a062453305579b35df
Joshua Neidich <joshua@reyoumassage.com>
2020-12-25 17:29:08 -0500

    added a header component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.page.jsx';
 import ShopPage from './pages/shop/shop.page.jsx';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
+    <div>
+          <Header />
     <Switch>
       <Route exact path="/" component={HomePage}/>
       <Route exact path="/shop" component={ShopPage}/>
     </Switch>
+    </div>
     
   );
 }
