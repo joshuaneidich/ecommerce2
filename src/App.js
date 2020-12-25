235736e271555573074b052840fc2640bdf1097c
Joshua Neidich <joshua@reyoumassage.com>
2020-12-25 17:15:02 -0500

    updating app with shop page and collectiion preview and collection item components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 
-import HomePage from './pages/homepage/homepage.component.jsx';
+import HomePage from './pages/homepage/homepage.page.jsx';
+import ShopPage from './pages/shop/shop.page.jsx';
 
 function App() {
   return (
     <Switch>
-      <Route exact={true} path="/" component={HomePage}/>
+      <Route exact path="/" component={HomePage}/>
+      <Route exact path="/shop" component={ShopPage}/>
     </Switch>
     
   );
