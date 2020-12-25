dea4fba2b1357f92eb88a41c2d1c33c884f83307
Joshua Neidich <joshua@reyoumassage.com>
2020-12-25 14:19:11 -0500

    added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
 
 function App() {
   return (
-    <HomePage />
+    <Switch>
+      <Route exact={true} path="/" component={HomePage}/>
+    </Switch>
+    
   );
 }
 
