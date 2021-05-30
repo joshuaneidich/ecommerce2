d8dbcadcd21d794e4ad996be9f8a7221483fa593
Joshua Neidich <joshua@reyoumassage.com>
2021-05-30 16:21:44 -0400

    changed logic of is loading to handle collections

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,18 +1,32 @@
 import ShopActionTypes from './shop.types';
 
 const INITIAL_STATE = {
-    collections: null
+    collections: null,
+    isFetching: false,
+    errorMessage: undefined
 }
 
 const shopReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case ShopActionTypes.UPDATE_COLLECTIONS:
+        case ShopActionTypes.FETCH_COLLECTIONS_START:
             return {
                 ...state,
-                collections: action.payload
+                isFetching: true
             }
-            default:
-                return state;
+            case ShopActionTypes.FETCH_COLLECTIONS_SUCCESS:
+                return {
+                    ...state,
+                    isFetching: false,
+                        collections: action.payload
+                }
+                case ShopActionTypes.FETCH_COLLECTIONS_Failure:
+                    return {
+                        ...state,
+                        isFetching: false,
+                            errorMessage: action.payload
+                    }
+                    default:
+                        return state;
     }
 }
 
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -17,3 +17,7 @@ export const selectCollection = collectionUrlParam => createSelector([selectColl
 export const selectCollectionsForPreview = createSelector([selectCollections],
     collections => collections ? Object.keys(collections).map(key => collections[key]) : []
 );
+
+export const selectIsCollectionFetching = createSelector([selectShop], shop => shop.isFetching);
+
+export const selectIsCollectionsLoaded = createSelector([selectShop], shop => !!shop.collections);
