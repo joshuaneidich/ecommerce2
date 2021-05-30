4c43d3d13b2b9035f8bf185d0f5661cf5944b6bc
Joshua Neidich <joshua@reyoumassage.com>
2021-05-29 22:49:32 -0400

    added in styled components

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -7,7 +7,6 @@ import CollectionPage from '../collection/collection.component';
 import './shop.styles.scss';
 
 const ShopPage = ({ match }) => {
-    console.dir(match)
     return (<div className="shop-page">
         <Route exact path={`${match.path}`} component={CollectionsOverview} />
         <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
