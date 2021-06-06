e0eabf730c1ba257073147dea6d0933c90ebf74b
Joshua Neidich <joshua@reyoumassage.com>
2021-06-06 13:56:10 -0400

    modified the structure of the app to include backend code

---
+++ client/src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,20 @@
+import React, { Profiler } from 'react';
+
+import Directory from '../../components/directory/directory.component';
+
+import { HomePageContainer } from './homepage.styles';
+
+const HomePage = () => (
+    <HomePageContainer>
+        <Profiler id="homePage" onRender={(id, phase, actualDuration) => {
+            console.log({
+                id, phase, actualDuration
+            })
+        }}>
+            <Directory />
+        </Profiler>
+    </HomePageContainer>
+
+)
+
+export default HomePage;
